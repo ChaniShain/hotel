6c44f4ae5d4c9f3745d6f5375d82fe94ec9de21f
ChaniShain <estrzon@gmail.com>
2023-11-26 15:36:47 +0200

    file arrangement

---
+++ Client/src/components/notFound.tsx
@@ -0,0 +1,26 @@
+import { Typography } from "@mui/material"
+
+
+export const NotFound = () => {
+
+
+
+    return (
+
+        <div>
+            <Typography variant="h4" align="center" sx={{ marginTop: "60px" }}>
+                Sorry
+            </Typography>
+            <Typography variant="h1" align="center" sx={{ marginTop: "60px" }}>
+                404
+            </Typography>
+            <hr />
+            <Typography variant="h3" align="center" sx={{ marginTop: "60px" }}>
+                PAGE NOT FOUND
+            </Typography></div>
+
+
+    )
+
+
+}
